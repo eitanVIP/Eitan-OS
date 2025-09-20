52e4ff383b387641c7b5f1c5dce5164d02c8590d
Eitan <eitangrimblat@gmail.com>
2025-09-20 14:56:37 +0300

    Made os run with GRUB and QEMU. Added basic VGA printing code.

--- build_run.sh
+++ build_run.sh
@@ -4,7 +4,7 @@ set -e
 # Files
 KERNEL_C="src/kernel.c"
 START_ASM="src/start.S"
-LINKER_SCRIPT="src/linker.ld"
+LINKER_SCRIPT="linker.ld"
 KERNEL_ELF="build/kernel.elf"
 
 # Clean and create another build folder
@@ -13,31 +13,32 @@ mkdir -p build
 
 # Assemble the startup assembly
 echo "[*] Assembling start.S..."
-nasm -f elf64 $START_ASM -o build/start.o
+nasm -f elf32 $START_ASM -o build/start.o
 
 # Compile kernel C code in freestanding mode
 echo "[*] Compiling kernel.c..."
-gcc -m64 -ffreestanding -nostdlib -c $KERNEL_C -o build/kernel.o
+gcc -m32 -fno-stack-protector -fno-builtin -ffreestanding -nostdlib -c $KERNEL_C -o build/kernel.o
 
 # Link the kernel into a single ELF file
 echo "[*] Linking kernel..."
-ld -n -o $KERNEL_ELF -T $LINKER_SCRIPT build/start.o build/kernel.o
+ld -n -m elf_i386 -o $KERNEL_ELF -T $LINKER_SCRIPT build/start.o build/kernel.o
 
 # Create ISO folder for GRUB
 mkdir -p build/iso/boot/grub
 cp $KERNEL_ELF build/iso/boot/kernel.elf
 cat > build/iso/boot/grub/grub.cfg <<EOF
 set timeout=0
-menuentry "eitanos64" {
-    multiboot2 /boot/kernel.elf
+menuentry "eitanos" {
+    multiboot /boot/kernel.elf
     boot
 }
 EOF
 
 # Make ISO with GRUB
 echo "[*] Creating ISO..."
-grub-mkrescue -o build/eitanos64.iso build/iso
+grub-mkrescue -o build/eitanos.iso build/iso
 
 # Launch in QEMU
 echo "[*] Launching QEMU..."
-qemu-system-x86_64 -cdrom build/eitanos64.iso -m 512 -serial stdio -boot d
+#qemu-system-x86_64 -cdrom build/eitanos.iso -m 512 -serial stdio -boot d
+qemu-system-i386 build/eitanos.iso
--- src/kernel.c
+++ src/kernel.c
@@ -2,13 +2,49 @@
 // Created by eitan on 9/19/25.
 //
 
-void kernel_main(void) {
-    volatile unsigned short* VGA = (unsigned short*)0xB8000;
-    const char* msg = "Hello from 64-bit kernel!";
-    for (int i = 0; msg[i]; i++) {
-        VGA[i] = (0x0F00 | msg[i]); // white text on black background
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+volatile unsigned short* VGA = (unsigned short*)0xB8000;
+int cursor_x = 0;
+int cursor_y = 0;
+
+void put_char(char c, int x, int y) {
+    VGA[y * VGA_WIDTH + x] = 0x0F00 | c;
+}
+
+void print(const char* msg, int size) {
+    for (int i = 0; i < size; i++) {
+        if (msg[i] == '\n') {
+            cursor_x = 0;
+            cursor_y++;
+        } else {
+            put_char(msg[i], cursor_x, cursor_y);
+            cursor_x++;
+            if (cursor_x >= VGA_WIDTH) {
+                cursor_x = 0;
+                cursor_y++;
+            }
+        }
     }
+
+    cursor_x = 0;
+    cursor_y++;
+}
+
+void clear_screen() {
+    for (int i = 0; i < VGA_HEIGHT * VGA_WIDTH; i++) {
+        VGA[i] = 0;
+    }
+}
+
+void kernel_main(void) {
+    clear_screen();
+    const char* msg = "Hello";
+    for (int j = 0; j < 10; j++)
+        print(msg, 5);
+
     while (1) {
-        asm volatile("hlt"); // halt CPU
+        asm volatile("hlt");
     }
 }
